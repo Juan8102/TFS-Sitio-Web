33dab2f131fe5f727a749e1e0b002bbd2393e3ba
Juan de Dios <125612729+Juan8102@users.noreply.github.com>
2025-11-19 03:42:51 -0300

    se espera el ultimo cambio para la visualizacion de la pagina

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'tfs-website';
+const repoName = 'TFS-Sitio-Web';
 
 // https://vite.dev/config/
 export default defineConfig({
