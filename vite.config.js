5f4cbd114d9543b7b8a8249a4d38cc6d36ece246
Juan de Dios <125612729+Juan8102@users.noreply.github.com>
2025-11-19 14:11:17 -0300

    buildeo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'TFS-Sitio-Web';
+const repoName = 'tfs-website';
 
 // https://vite.dev/config/
 export default defineConfig({
